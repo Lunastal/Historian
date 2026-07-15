6fff2a7e760e91765e23282dd9e3de5b6a356655
Lunastal <305340457+Lunastal@users.noreply.github.com>
2026-07-15 05:44:37 +0000

    Initial commit
    Signed-off-by: Lunastal <305340457+Lunastal@users.noreply.github.com>

---
+++ output.js
@@ -0,0 +1,13 @@
+// Checkout the Guidebook examples to get an idea of other ways you can use scripting
+// https://help.aidungeon.com/scripting
+
+const modifier = (text) => {
+  // Single module, always overwrite
+  let ret = Historian.getOutputHookText(text);
+  text = ret.text;
+  
+  return { text }
+}
+
+// Don't modify this part
+modifier(text)
